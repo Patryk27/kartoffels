4ebe61693a892c06b388a0e0fa75ce165f6e59ce
Patryk Wychowaniec <pwychowaniec@pm.me>
2024-09-02 20:18:00 +0200

    web: Switch to `Fira Code`, use WebGL terminal

--- web/app.js
+++ web/app.js
@@ -1,6 +1,7 @@
 import { Terminal } from "@xterm/xterm";
 import { AttachAddon } from "@xterm/addon-attach";
 import { FitAddon } from "@xterm/addon-fit";
+import { WebglAddon } from "@xterm/addon-webgl";
 import "@xterm/xterm/css/xterm.css";
 
 const $app = document.getElementById("app");
@@ -9,8 +10,8 @@ document.fonts.ready.then(() => {
   const term = new Terminal({
     rows: 0,
     cols: 0,
-    fontSize: 26,
-    fontFamily: "Inconsolata",
+    fontSize: 28,
+    fontFamily: "Fira Code",
   });
 
   const termFit = new FitAddon();
@@ -18,16 +19,17 @@ document.fonts.ready.then(() => {
   term.open($app);
   term.write("connecting...");
   term.loadAddon(termFit);
+  term.loadAddon(new WebglAddon());
 
-  const socket = new WebSocket("ws://localhost:1313");
-
-  term.loadAddon(
-    new AttachAddon(socket, {
-      bidirectional: true,
-    }),
-  );
+  const socket = new WebSocket("ws:localhost:1313");
 
   socket.onopen = () => {
+    term.loadAddon(
+      new AttachAddon(socket, {
+        bidirectional: true,
+      }),
+    );
+
     term.onResize((event) => {
       const packet = new Uint8Array(3);
 
@@ -49,4 +51,44 @@ document.fonts.ready.then(() => {
       termFit.fit();
     }, 100);
   };
+
+  socket.onmessage = (event) => {
+    const msg = new Uint8Array(event.data);
+
+    if (msg.length == 1 && msg[0] == 0x04) {
+      handleBotCreate();
+    }
+  };
+
+  socket.onclose = () => {
+    setTimeout(() => {
+      term.reset();
+
+      term.write(
+        "error: lost connection to the server - try refreshing the page",
+      );
+    }, 150);
+  };
+
+  window.onbeforeunload = () => {
+    socket.onclose = null;
+  };
+
+  function handleBotCreate() {
+    const input = document.createElement("input");
+
+    input.type = "file";
+
+    input.onchange = (event) => {
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        term.paste(btoa(new Uint8Array(reader.result)));
+      };
+
+      reader.readAsArrayBuffer(event.target.files[0]);
+    };
+
+    input.click();
+  }
 });
