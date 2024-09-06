e35293233ae33a6c47bf05381db22e73afa37f3e
Patryk Wychowaniec <pwychowaniec@pm.me>
2024-09-06 12:07:46 +0200

    game/upload-bot: Show an alert on Ctrl+V

--- crates/kartoffels-game/src/views/play/dialog/upload_bot.rs
+++ crates/kartoffels-game/src/views/play/dialog/upload_bot.rs
@@ -4,8 +4,11 @@ use ratatui::style::Stylize;
 use ratatui::text::{Line, Span};
 use ratatui::widgets::{Paragraph, Widget, WidgetRef, Wrap};
 use std::cmp;
+use std::pin::Pin;
 use std::sync::LazyLock;
-use termwiz::input::{InputEvent, KeyCode};
+use std::time::Duration;
+use termwiz::input::{InputEvent, KeyCode, Modifiers};
+use tokio::time;
 
 static TEXT: LazyLock<Paragraph<'static>> = LazyLock::new(|| {
     Paragraph::new(vec![
@@ -28,6 +31,7 @@ static TEXT: LazyLock<Paragraph<'static>> = LazyLock::new(|| {
 #[derive(Debug, Default)]
 pub struct UploadBotDialog {
     spinner: Spinner,
+    ctrlv_alert: Option<Pin<Box<time::Sleep>>>,
 }
 
 impl UploadBotDialog {
@@ -36,15 +40,25 @@ impl UploadBotDialog {
 
         if ui.ty().is_ssh() {
             let width = cmp::min(ui.area().width - 10, 60);
-            let lines = TEXT.line_count(width) as u16;
-            let height = lines + 4;
+            let text_height = TEXT.line_count(width) as u16;
 
             let spinner = self.spinner.as_span(ui);
+
+            if ui.key(KeyCode::Char('v'), Modifiers::CTRL) {
+                self.ctrlv_alert =
+                    Some(Box::pin(time::sleep(Duration::from_secs(3))));
+            }
+
+            let height = if self.ctrlv_alert.is_some() {
+                text_height + 6
+            } else {
+                text_height + 4
+            };
 
             ui.info_dialog(width, height, Some(" uploading a bot "), |ui| {
                 TEXT.render_ref(ui.area(), ui.buf());
 
-                ui.space(lines + 1);
+                ui.space(text_height + 1);
 
                 Line::from_iter([
                     spinner.clone(),
@@ -56,6 +70,21 @@ impl UploadBotDialog {
 
                 ui.space(2);
 
+                if let Some(alert) = &mut self.ctrlv_alert {
+                    ui.line(
+                        Line::raw("try using Ctrl+Shift+V instead of Ctrl+V")
+                            .fg(theme::POTATO)
+                            .bold()
+                            .centered(),
+                    );
+
+                    ui.space(1);
+
+                    if ui.poll(alert).is_ready() {
+                        self.ctrlv_alert = None;
+                    }
+                }
+
                 if Button::new(KeyCode::Escape, "cancel").render(ui).pressed {
                     resp = Some(DialogResponse::Close);
                 }
@@ -65,7 +94,7 @@ impl UploadBotDialog {
         if let Some(InputEvent::Paste(src)) = ui.event() {
             if src.is_empty() {
                 // A bit hacky, but that's the most sane way for the http
-                // frontend to inform us that user's cancelled the uploading
+                // frontend to inform us that user has cancelled the uploading
                 resp = Some(DialogResponse::Close);
             } else {
                 resp = Some(DialogResponse::UploadBot(src.to_owned()));
