16461462e2d12cf1c42462b54a596a5af6110211
Patryk Wychowaniec <pwychowaniec@pm.me>
2024-10-07 18:59:19 +0200

    world: Log firmware hashes

--- backend/crates/kartoffels-world/src/bots/systems/create.rs
+++ backend/crates/kartoffels-world/src/bots/systems/create.rs
@@ -4,6 +4,7 @@ use glam::IVec2;
 use kartoffels_cpu::Cpu;
 use std::borrow::Cow;
 use std::sync::Arc;
+use tracing::info;
 
 pub fn run(
     world: &mut World,
@@ -11,6 +12,9 @@ pub fn run(
     pos: Option<IVec2>,
 ) -> Result<BotId> {
     let cpu = Cpu::new(&src).context("couldn't parse firmware")?;
+    let src_hash = sha256::digest(&src[..]);
+
+    info!(?src_hash, "creating bot");
 
     let id = loop {
         let id = BotId::new(&mut world.rng);
