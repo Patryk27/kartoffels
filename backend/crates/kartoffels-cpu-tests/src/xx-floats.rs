71d052503ed44444272c54126d315c03c3253d90
Patryk Wychowaniec <pwychowaniec@pm.me>
2024-10-20 14:39:05 +0200

    cpu: Add a test for f32

---
+++ backend/crates/kartoffels-cpu-tests/src/xx-floats.rs
@@ -0,0 +1,26 @@
+#![cfg_attr(target_arch = "riscv64", no_std, no_main)]
+
+extern crate kartoffel;
+
+use core::hint::black_box;
+
+#[cfg_attr(target_arch = "riscv64", no_mangle)]
+fn main() {
+    let mut out = 0.0f32;
+
+    for x in 0..128 {
+        for y in 64..256 {
+            out = black_box(out);
+            out += x as f32;
+
+            out = black_box(out);
+            out *= y as f32;
+        }
+    }
+
+    kartoffels_cpu_tests::exit(out.to_bits());
+}
+
+/*
+ * x10 = 2139095040
+ */
