b47dc4747eec26645225b7d4a34da3f9c5c8c8f9
Patryk Wychowaniec <pwychowaniec@pm.me>
2024-11-15 20:13:14 +0100

    kartoffel: Add docs for `arm_pick()` and `arm_drop()`

--- backend/crates/kartoffel/src/arm.rs
+++ backend/crates/kartoffel/src/arm.rs
@@ -39,8 +39,10 @@ pub fn arm_wait() {
     }
 }
 
-/// Stabs the bot in front of you, killing it; if there's no bot there, nothing
-/// happens.
+/// Stabs the bot in front of you, killing it.
+///
+/// If there's no bot there, nothing happens (but the cooldown is still
+/// applied).
 ///
 /// # Cooldown
 ///
@@ -61,14 +63,63 @@ pub fn arm_stab() {
     wri(MEM_ARM, 0, 1);
 }
 
-// TODO
+/// Picks the object in front of you and puts it into the inventory under the
+/// zeroth index, shifting previously-picked objects into further indices.
+///
+/// (that is: if you had object at idx=0, picking a new object would shift this
+/// already-picked object into idx=1, and then the newly-picked object would
+/// appear at idx=0.)
+///
+/// If there's no object in front of you or you don't have any more space in the
+/// inventory, nothing happens (but the cooldown is still applied).
+///
+/// # Cooldown
+///
+/// ```text
+/// 60_000 +- 15% ticks (~930 ms)
+/// ```
+///
+/// # Example
+///
+/// ```no_run
+/// # use kartoffel::*;
+/// #
+/// arm_wait();
+/// arm_pick();
+/// ```
 #[inline(always)]
 pub fn arm_pick() {
     wri(MEM_ARM, 0, 2);
 }
 
-// TODO
+/// Takes object from the inventory and drops it in front of you, shifting
+/// following objects into their previous indices.
+///
+/// (that is: if you had objects at idx=0, idx=1 and idx=2, then dropping the
+/// object at idx=1 would move object idx=2 back to idx=1.)
+///
+/// If you don't have object with given index or the object can't be placed in
+/// front of you (e.g. because you're looking at a wall), nothing hapens (but
+/// the cooldown is still applied).
+///
+/// # Cooldown
+///
+/// ```text
+/// 60_000 +- 15% ticks (~930 ms)
+/// ```
+///
+/// # Example
+///
+/// ```no_run
+/// # use kartoffel::*;
+/// #
+/// arm_wait();
+/// arm_pick();
+///
+/// arm_wait();
+/// arm_drop(0); // drops whatever got picked before
+/// ```
 #[inline(always)]
-pub fn arm_drop(nth: u8) {
-    wri(MEM_ARM, 0, u32::from_be_bytes([0, 0, nth, 3]));
+pub fn arm_drop(idx: u8) {
+    wri(MEM_ARM, 0, u32::from_be_bytes([0, 0, idx, 3]));
 }
