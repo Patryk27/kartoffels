8686fc28acc2956b263c8561e03798a65fedbd3f
Patryk Wychowaniec <pwychowaniec@pm.me>
2025-01-20 20:05:44 +0100

    kartoffel: Mention that timer overflows after 18h

--- app/crates/kartoffel/src/timer.rs
+++ app/crates/kartoffel/src/timer.rs
@@ -4,13 +4,16 @@ use crate::{rdi, MEM_TIMER};
 /// for hashmaps and the like.
 ///
 /// Note that this doesn't return a *new* random number each time it's called -
-/// rather the number is randomized once, when the bot is being (re)started.
+/// the number is randomized once, when the bot is born.
 #[inline(always)]
 pub fn timer_seed() -> u32 {
     rdi(MEM_TIMER, 0)
 }
 
 /// Returns the number of ticks that have passed since the bot's been born.
+///
+/// This counter overflows after about 18 hours, after which it will start
+/// counting from zero.
 #[inline(always)]
 pub fn timer_ticks() -> u32 {
     rdi(MEM_TIMER, 1)
