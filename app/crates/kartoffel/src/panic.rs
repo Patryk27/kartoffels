8d67329c4e2ec197bdc12a0a3ae2af5bd7c2089a
Patryk Wychowaniec <pwychowaniec@pm.me>
2025-01-25 13:53:12 +0100

    kartoffel/panic: Refactoring

--- app/crates/kartoffel/src/panic.rs
+++ app/crates/kartoffel/src/panic.rs
@@ -3,17 +3,13 @@ use core::fmt::Write;
 use core::panic::PanicInfo;
 
 #[allow(dead_code)]
-#[allow(clippy::empty_loop)]
 #[cfg_attr(target_arch = "riscv64", panic_handler)]
-fn panic(i: &PanicInfo) -> ! {
-    let mut output = Serial;
-
-    // Only print the panic message if the `serial-panic` feature is enabled.
-    // Allows it to be disabled for smaller binaries.
+fn panic(info: &PanicInfo) -> ! {
     #[cfg(feature = "serial-panic")]
-    let _ = write!(&mut output, "\n{}", i);
+    let _ = write!(&mut Serial, "\n{info}");
 
+    #[allow(clippy::empty_loop)]
     loop {
-        // Loop forever
+        //
     }
 }
