b4a45714c8be94b80738642ba68d0501996a8ae4
Patryk Wychowaniec <pwychowaniec@pm.me>
2025-03-12 16:19:30 +0100

    kartoffel: Skip build.rs on non-riscv32 archs

--- app/crates/kartoffel/build.rs
+++ app/crates/kartoffel/build.rs
@@ -1,10 +1,13 @@
+use std::env;
 use std::path::Path;
 
 fn main() {
-    println!(
-        "cargo:rustc-link-search={}",
-        Path::new(file!()).parent().unwrap().display(),
-    );
+    if env::var("CARGO_CFG_TARGET_ARCH").as_deref() == Ok("riscv32") {
+        println!(
+            "cargo:rustc-link-search={}",
+            Path::new(file!()).parent().unwrap().display(),
+        );
 
-    println!("cargo:rerun-if-changed=kartoffel.ld");
+        println!("cargo:rerun-if-changed=kartoffel.ld");
+    }
 }
